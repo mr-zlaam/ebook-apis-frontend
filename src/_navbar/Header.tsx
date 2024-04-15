130eeadf882524abeb6233ce0cac69d5d35b9515
Zlaam <mrzalaam@gmail.com>
2024-04-15 11:39:41 +0500

    update Header.tsx and MobileNavItems.tsx: add mobile responsive navbar

--- src/_navbar/Header.tsx
+++ src/_navbar/Header.tsx
@@ -1,19 +1,48 @@
+"use client";
 import Logo from "@/_subComponents/logo";
-import {} from "react";
+import { Menu, X } from "lucide-react";
+import { useState } from "react";
+import MobileNavItems from "./MobileNavItems";
 import NavItems from "./NavItems";
-import { Menu } from "lucide-react";
-//TODO: Make Responsive Navbar
+import { cn } from "@/lib/utils";
 function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const handleShowNavBar = () => {
+    setIsMenuOpen((prev) => !prev);
+  };
   return (
     <>
       <header className="flex justify-around items-center h-[70px]  sticky top-0 bg-background shadow">
-        <div>
+        <div className="">
+          {isMenuOpen && (
+            <X
+              className="absolute left-10 z-50 text-foreground"
+              onClick={() => {
+                setIsMenuOpen(false);
+              }}
+            />
+          )}{" "}
           <Logo />
         </div>
         <nav className="hidden z-50 lg:flex lg:self-stretch  gap-10 items-center ">
           <NavItems />
         </nav>
-        <Menu className="block lg:hidden" />
+        {!isMenuOpen && (
+          <Menu className="inline lg:hidden" onClick={handleShowNavBar} />
+        )}
+        <MobileNavItems
+          className={cn(
+            !isMenuOpen
+              ? "translate-y-[-120%] duration-300 "
+              : "translate-y-0 duration-300"
+          )}
+        />
+        {isMenuOpen && (
+          <div
+            onClick={() => setIsMenuOpen(false)}
+            className="before:content[''] before:absolute before:top-0 before:left-0 before:h-screen before:w-full before:bg-black/30 before:backdrop-blur-sm"
+          />
+        )}
       </header>
     </>
   );
--- src/_navbar/MobileNavItems.tsx
+++ src/_navbar/MobileNavItems.tsx
@@ -1,11 +1,20 @@
 import {} from "react";
+import NavItems from "./NavItems";
+import { cn } from "@/lib/utils";
 
-function MobileNavBar() {
+function MobileNavItems({ className }: { className?: string }) {
   return (
     <>
-      <nav>MobileNavBar</nav>
+      <nav
+        className={cn(
+          "flex flex-col bg-background shadow-gray-900 shadow-lg h-[400px] items-center justify-around w-[250px] absolute z-50 right-2 top-2  rounded-lg md:w-[300px]",
+          className
+        )}
+      >
+        <NavItems />
+      </nav>
     </>
   );
 }
 
-export default MobileNavBar;
+export default MobileNavItems;
