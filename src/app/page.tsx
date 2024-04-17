6a95e75eb48a767307ede1cc9a3708280e7814b2
Zlaam <mrzalaam@gmail.com>
2024-04-17 12:10:11 +0500

    update: make mobile responsive hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,21 @@
 import Container from "@/_container/Container";
 import Image from "next/image";
-import heroImage from "../../images/hero.jpg";
+import heroSectionImage from "../../images/hero.jpg";
 
 export default function Home() {
   //TODO:RESUME FROM HERE NEXT TIME
   return (
     <Container className="mt-2 flex flex-col items-center justify-center ">
-      <Image
-        className="rounded-lg object-cover "
-        src={heroImage}
-        alt="hero"
-        width={1000}
-      />
-      <h1 className="font-bold text md:text-5xl lg:text-7xl text-start lg:ml-16 text-3xl   ">
-        UNLEASH THE POWER OF IMAGINATION.
-      </h1>
+      <div className="h-[87vh] md:h-[90vh] overflow-x-hidden">
+        <Image
+          className="rounded-lg object-cover h-[75%] md:h-[79%] w-full overflow-x-hidden md:drop-shadow-xl select-none "
+          src={heroSectionImage}
+          alt="hero"
+        />
+        <h1 className="font-bold text md:text-5xl lg:text-7xl text-start lg:ml-16 text-4xl   ">
+          UNLEASH THE POWER OF IMAGINATION.
+        </h1>
+      </div>
     </Container>
   );
 }
