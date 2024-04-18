e5f91452161e3ec5c12b1d8ae66199a5a2dd012d
Zlaam <mrzalaam@gmail.com>
2024-04-18 14:33:16 +0500

    add:  check the length of the data

--- src/app/books/components/BookList/BookList.tsx
+++ src/app/books/components/BookList/BookList.tsx
@@ -2,32 +2,43 @@ import { cn } from "@/lib/utils";
 import { BookTypes } from "@/types";
 import Image from "next/image";
 //TODO: Resume from here
-const fetchBookList = async () => {
-  const response = await fetch(`http://localhost:5173/api/books`);
+const fetchBookList = async (BACKEND_URI: string) => {
+  const response = await fetch(`${BACKEND_URI}/books`, {
+    cache: "no-store",
+  });
   const data = await response.json();
   return data;
 };
 
 async function BookList() {
-  // const { BACKEND_URI } = process.env;
-  const data = await fetchBookList();
+  const { BACKEND_URI } = process.env;
+  const data = await fetchBookList(BACKEND_URI as string);
   const { data: bookData } = data;
-  console.log(bookData);
   const cardClass = "rounded-lg h-[250px] w-[380px]  ";
+  if (bookData?.books.length === 0) {
+    return (
+      <h1 className="h-[300px]  flex justify-center items-center text-4xl font-bold text-red-500">
+        No result Found!
+      </h1>
+    );
+  }
   return (
     <>
       <section className="md:grid md:grid-cols-2 md:gap-5 lg:grid lg:grid-cols-3 lg:gap-10 grid grid-col-1 gap-3 justify-center">
         {bookData.books.map((book: BookTypes) => {
           return (
             <div
-              className={cn("shadow-black/30  shadow-md flex", cardClass)}
+              className={cn(
+                "shadow-md overflow-hidden shadow-black/40 flex items-center justify-around border border-white/40",
+                cardClass
+              )}
               key={book?._id}
             >
               <Image
                 src={book?.coverImage}
                 alt={book?.author?.displayName}
-                height={100}
-                width={100}
+                height={200}
+                width={200}
               />
               <h1>{book.author.displayName}</h1>
             </div>
