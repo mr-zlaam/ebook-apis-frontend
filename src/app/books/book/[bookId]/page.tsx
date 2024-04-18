961828969e436a7b327edd48f9c0ab7dd89ff2fb
Zlaam <mrzalaam@gmail.com>
2024-04-18 22:37:10 +0500

    update page.tsx

--- src/app/books/book/[bookId]/page.tsx
+++ src/app/books/book/[bookId]/page.tsx
@@ -1,13 +1,14 @@
 import Container from "@/_container/Container";
 import { BookTypes } from "@/types";
 import Image from "next/image";
-import Link from "next/link";
 import {} from "react";
 import ReadLink from "../../components/readBook/ReadLink";
 const fetchSignleBook = async (bookid: string) => {
   let GetBook: BookTypes | null = null;
   try {
-    const response = await fetch(`${process.env.BACKEND_URI}/books/${bookid}`);
+    const response = await fetch(`${process.env.BACKEND_URI}/books/${bookid}`, {
+      cache: "no-store",
+    });
     if (!response.ok) {
       return;
     } else {
@@ -29,27 +30,16 @@ async function BookIdPage({ params }: { params: { bookId: string } }) {
   }
   return (
     <>
-      <Container className="mt-5">
+      <Container className="mt-5 relative">
         <div className="flex flex-col items-center justify-center lg:flex lg:flex-row lg:items-start lg:justify-around">
           <div className="lg:text-6xl font-bold text-balance flex-1 sm:text-5xl sm:my-10 text-3xl my-5">
-            The Phsycology of money.
+            {singleBook?.title}
             <p className=" text-center text-sm my-4">
               By:
               <span className="text-orange-800 font-fugaz text-xl"> Zlaam</span>
             </p>
             <p className="text-sm font-semibold leading-6 mx-4 text-left max-w-[80%] tracking-wide">
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor
-              non architecto omnis itaque temporibus nostrum, nemo quo dolorem
-              sit vero facilis optio velit asperiores, repellat similique
-              dolorum, sapiente nam illum! Earum explicabo eos nisi unde
-              voluptas at? Labore, sapiente vero enim officiis molestiae numquam
-              quasi optio itaque dicta nam est molestias aliquam? Porro nostrum
-              tenetur numquam, maiores quaerat impedit eius omnis deleniti
-              eveniet velit dolorum aspernatur necessitatibus, cumque est saepe
-              aliquam in distinctio dolore sint labore illo quo. Esse quidem
-              repellendus commodi! Iusto, ad totam esse provident facilis nisi
-              dolorem est veritatis fuga magnam, assumenda quaerat, soluta
-              excepturi earum quas?
+              {singleBook?.description}
             </p>
           </div>
 
@@ -65,7 +55,7 @@ async function BookIdPage({ params }: { params: { bookId: string } }) {
             )}
           </div>
         </div>
-        <div className="w-fit mx-auto">
+        <div className="w-fit mx-auto my-10">
           {singleBook?.file && <ReadLink file={singleBook.file} />}
         </div>
       </Container>
