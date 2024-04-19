4777e6ee8e1894c02415fb05a86a0406862a7d03
Zlaam <mrzalaam@gmail.com>
2024-04-19 20:05:14 +0500

    add : label

--- src/app/user/components/form.tsx
+++ src/app/user/components/form.tsx
@@ -1,3 +1,6 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/lable";
 import {} from "react";
 
 function Form() {
@@ -6,72 +9,34 @@ function Form() {
       <section className="relative top-20">
         <form>
           <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto lg:py-0">
-            <div className="w-full bg-white rounded-lg shadow border md:mt-0 sm:max-w-md xl:p-0">
+            <div className="w-full bg-background rounded-lg shadow border md:mt-0 sm:max-w-md xl:p-0">
               <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
                 <h1 className="text-xl text-center font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
                   Sign in
                 </h1>
-                <div>
-                  <label className="block mb-2 text-sm font-medium text-gray-900">
-                    Your username
-                  </label>
-                  <input
-                    placeholder="JohnDoe"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg block w-full p-2.5"
-                    id="username"
-                    type="text"
-                  />
+                <div className="grid w-full max-w-sm items-center gap-1.5">
+                  <Label htmlFor="username">Username</Label>
+                  <Input type="text" id="username" placeholder="john_doe" />
                 </div>
-                <div>
-                  <label className="block mb-2 text-sm font-medium text-gray-900">
-                    Password
-                  </label>
-                  <input
-                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg block w-full p-2.5"
-                    placeholder="••••••••"
-                    id="password"
-                    type="password"
-                  />
+                <div className="grid w-full max-w-sm items-center gap-1.5">
+                  <Label htmlFor="fullname">Full Name</Label>
+                  <Input type="text" id="fullname" placeholder="John Doe" />
                 </div>
-                <div>
-                  <label className="block mb-2 text-sm font-medium text-gray-900">
-                    Confirm password
-                  </label>
-                  <input
-                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg block w-full p-2.5"
-                    placeholder="••••••••"
-                    id="confirmPassword"
-                    type="password"
-                  />
+                <div className="grid w-full max-w-sm items-center gap-1.5">
+                  <Label htmlFor="email">Email</Label>
+                  <Input type="email" id="email" placeholder="john@mail.com" />
                 </div>
-                <div className="flex items-start">
-                  <div className="flex items-center h-5">
-                    <input
-                      className="w-4 h-4 border  rounded  focus:ring-3 focus:ring-primary-300 bg-gray-700 border-gray-600 focus:ring-primary-600 ring-offset-gray-800"
-                      type="checkbox"
-                      aria-describedby="terms"
-                      id="terms"
-                    />
-                  </div>
-                  <div className="ml-3 text-sm">
-                    <label className="font-light text-gray-500 ">
-                      I accept the
-                      <a
-                        href="#"
-                        className="font-medium text-primary-600 hover:underline text-primary-500"
-                      >
-                        Terms and Conditions
-                      </a>
-                    </label>
-                  </div>
+                <div className="grid w-full max-w-sm items-center gap-1.5">
+                  <Label htmlFor="password">Password</Label>
+                  <Input type="password" id="password" placeholder="••••••••" />
                 </div>
-
-                <button
-                  className="w-full bg-blue-500 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center  focus:ring-blue-800 text-white"
-                  type="submit"
-                >
-                  Create an account
-                </button>
+                <div className="grid w-full max-w-sm items-center gap-1.5">
+                  <Label htmlFor="confirm">Confirm Password</Label>
+                  <Input type="password" id="confirm" placeholder="••••••••" />
+                </div>
+                <Button className="w-full bg-blue-500 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center  focus:ring-blue-800 text-white">
+                  Sign Up
+                </Button>
               </div>
             </div>
           </div>
