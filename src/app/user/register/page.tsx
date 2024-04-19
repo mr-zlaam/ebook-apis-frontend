bacdc7a4b73e6a3d3e10fb5247bae9606abf37aa
Zlaam <mrzalaam@gmail.com>
2024-04-19 19:35:08 +0500

    add : add register form

---
+++ src/app/user/register/page.tsx
@@ -0,0 +1,14 @@
+import {} from "react";
+import Form from "../components/form";
+
+function Register() {
+  return (
+    <>
+      <section>
+        <Form />
+      </section>
+    </>
+  );
+}
+
+export default Register;
