81f7b8e5e92b61c90c03b02e436ac612be0d19a1
Zlaam <mrzalaam@gmail.com>
2024-04-19 19:30:49 +0500

    feat: create page.tsx: add user form

---
+++ src/app/user/login/page.tsx
@@ -0,0 +1,14 @@
+import {} from "react";
+import Form from "../components/form";
+
+function Login() {
+  return (
+    <>
+      <section>
+        <Form />
+      </section>
+    </>
+  );
+}
+
+export default Login;
