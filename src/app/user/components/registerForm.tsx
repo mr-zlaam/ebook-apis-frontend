d40d6659b05697d4c5c54bd38746dc4e5043fb54
Zlaam <mrzalaam@gmail.com>
2024-04-24 01:02:12 +0500

    update registerForm.tsx and postSchema.ts

--- src/app/user/components/registerForm.tsx
+++ src/app/user/components/registerForm.tsx
@@ -11,7 +11,7 @@ function RegisterForm() {
   const {
     register,
     handleSubmit,
-    formState: { errors, isSubmitting },
+    formState: { errors },
   } = useForm<UserRegisterTypes>({ resolver: zodResolver(registerSchema) });
   const handleRegisterSubmit = async (data: UserRegisterTypes) => {
     try {
@@ -21,26 +21,27 @@ function RegisterForm() {
   };
   return (
     <>
-      <section className="relative top-20">
+      <section className="relative top-8">
         <form onSubmit={handleSubmit(handleRegisterSubmit)}>
-          <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto lg:py-0">
+          <div className="flex flex-col items-center justify-center px-6 py-8  mx-auto lg:py-0">
             <div className="w-full bg-background rounded-lg shadow border md:mt-0 sm:max-w-md xl:p-0">
-              <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+              <div className="p-6  space-y-4 md:space-y-6 sm:p-8">
                 <h1 className="text-xl text-center font-bold leading-tight tracking-tight text-foreground md:text-2xl">
                   Create an Account
                 </h1>
                 <div className="grid w-full max-w-sm items-center gap-1.5">
                   <Label htmlFor="username">Username</Label>
                   <Input
+                    autoCapitalize="off"
                     {...register("username")}
                     type="text"
                     placeholder="john_doe"
+                    id="username"
                   />
                   {errors.username && (
-                    <span className="text-xs text-red-500">
-                      {" "}
-                      {errors.username.message}{" "}
-                    </span>
+                    <p className="text-xs select-none text-red-500 h-[15px] text-balance ml-2">
+                      {errors.username.message}
+                    </p>
                   )}
                 </div>
                 <div className="grid w-full max-w-sm items-center gap-1.5">
@@ -49,12 +50,12 @@ function RegisterForm() {
                     {...register("fullname")}
                     type="text"
                     placeholder="John Doe"
+                    id="fullname"
                   />
                   {errors.fullname && (
-                    <span className="text-xs text-red-500">
-                      {" "}
-                      {errors.fullname.message}{" "}
-                    </span>
+                    <p className="text-xs select-none text-red-500 h-[15px] text-balance ml-2">
+                      {errors.fullname.message}
+                    </p>
                   )}
                 </div>
                 <div className="grid w-full max-w-sm items-center gap-1.5">
@@ -63,12 +64,12 @@ function RegisterForm() {
                     {...register("email")}
                     type="email"
                     placeholder="john@mail.com"
+                    id="email"
                   />
                   {errors.email && (
-                    <span className="text-xs text-red-500">
-                      {" "}
-                      {errors.email.message}{" "}
-                    </span>
+                    <p className="text-xs select-none text-red-500 h-[15px] text-balance ml-2">
+                      {errors.email.message}
+                    </p>
                   )}
                 </div>
                 <div className="grid w-full max-w-sm items-center gap-1.5">
@@ -77,12 +78,12 @@ function RegisterForm() {
                     {...register("password")}
                     type="password"
                     placeholder="••••••••"
+                    id="password"
                   />
                   {errors.password && (
-                    <span className="text-xs text-red-500">
-                      {" "}
-                      {errors.password.message}{" "}
-                    </span>
+                    <p className="text-xs select-none text-red-500 h-[15px] text-balance ml-2">
+                      {errors.password.message}
+                    </p>
                   )}
                 </div>
                 <div className="grid w-full max-w-sm items-center gap-1.5">
@@ -91,12 +92,12 @@ function RegisterForm() {
                     {...register("confirmPassword")}
                     type="password"
                     placeholder="••••••••"
+                    id="confirm"
                   />
                   {errors.confirmPassword && (
-                    <span className="text-xs text-red-500">
-                      {" "}
+                    <p className="text-xs select-none text-red-500 h-[15px] text-balance ml-2">
                       {errors.confirmPassword.message}{" "}
-                    </span>
+                    </p>
                   )}
                 </div>
 
@@ -110,7 +111,7 @@ function RegisterForm() {
                     Login
                   </Link>
                 </p>
-                <Button className="w-full bg-blue-500 duration-200 text-white transition-all hover:bg-blue-700 ">
+                <Button className="w-full  duration-200 text-white transition-all hover:bg-blue-700 ">
                   Sign Up
                 </Button>
               </div>
--- src/validation/postSchema.ts
+++ src/validation/postSchema.ts
@@ -3,29 +3,29 @@ import { ZodType, object, string } from "zod";
 
 export const registerSchema: ZodType<UserRegisterTypes> = object({
   username: string()
-    .min(3, { message: "This field required at least  3 characters" })
+    .min(3, { message: "This field requires at least  3 characters" })
     .max(10, { message: "Username must not contain more than 10 characters" })
     .regex(new RegExp(/^[a-z0-9]+(_[a-z0-9]+)?(-[a-z0-9]+)?$/), {
       message: "Username is invalid.",
     })
     .toLowerCase(),
   fullname: string()
-    .min(3, { message: "This field required at least  3 characters" })
+    .min(3, { message: "This field requires at least  3 characters" })
     .max(20, { message: "Full Name is too long" })
     .regex(new RegExp(/^[a-zA-Z0-9]+$/), { message: "Full name is invalid" }),
   email: string()
     .email()
-    .min(5, { message: "This field required at least 5 characters" })
+    .min(5, { message: "This field requires at least 5 characters" })
     .max(20, { message: "Email is too long" })
     .regex(new RegExp(/^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/), {
       message: "Please provide valid email",
     })
     .toLowerCase(),
   password: string()
-    .min(6, { message: "This field required at least 6 characters" })
+    .min(6, { message: "This field requires at least 6 characters" })
     .max(20, { message: "Password is too long" }),
   confirmPassword: string()
-    .min(6, { message: "This field required at least 6 characters" })
+    .min(6, { message: "This field requires at least 6 characters" })
     .max(20, { message: "Password is too long" }),
 }).refine((data) => data.password === data.confirmPassword, {
   message: "Confirmpassword is not same as passwords",
