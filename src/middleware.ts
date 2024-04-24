7afe7416bd83e1a69a3d24c55c1265ad90b23873
Zlaam <mrzalaam@gmail.com>
2024-04-24 15:56:40 +0500

    update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,7 +6,7 @@ export function middleware(request: NextRequest) {
   const biscuitCookieStore = cookies();
   const token = biscuitCookieStore.get("accessToken");
   console.log(token?.value);
-  if (!token?.value || !token?.name || !token) {
+  if (!token?.value || !token?.name || !token || token.value.length < 172) {
     return NextResponse.redirect(new URL("/user/login", request.url));
   } else {
     return NextResponse.next();
