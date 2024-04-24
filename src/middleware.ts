292ca7e261060e63edd959450250fd97a527de70
Zlaam <mrzalaam@gmail.com>
2024-04-24 16:08:50 +0500

    update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,9 @@
 import type { NextRequest } from "next/server";
 import { cookies } from "next/headers";
-
 import { NextResponse } from "next/server";
 export function middleware(request: NextRequest) {
   const biscuitCookieStore = cookies();
   const token = biscuitCookieStore.get("accessToken");
-  console.log(token?.value);
   if (!token?.value || !token?.name || !token || token.value.length < 172) {
     return NextResponse.redirect(new URL("/user/login", request.url));
   } else {
