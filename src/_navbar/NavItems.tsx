3c177d20fc5f4c56ff77d5ae93c5bf49df109970
Zlaam <mrzalaam@gmail.com>
2024-04-25 16:40:54 +0500

    update NavItems.tsx

--- src/_navbar/NavItems.tsx
+++ src/_navbar/NavItems.tsx
@@ -4,9 +4,8 @@ import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { DeleteBiscuitCookie } from "@/hooks/useCookies";
 import { useEffect, useState } from "react";
-import { getCookie } from "cookies-next";
+import { getCookie, deleteCookie } from "cookies-next";
 
-import axios from "axios";
 interface FetchUserResponseType {
   _id: string;
   username: string;
@@ -21,40 +20,22 @@ const linkClass: string =
 ///  NavItems here *****************
 function NavItems({ handleCloseNavbar }: { handleCloseNavbar: () => void }) {
   const router = useRouter();
-  const [currentUser, setCurrentUser] = useState<
-    null | FetchUserResponseType[]
-  >(null);
+  const [isUserLogined, setIsUserLogined] = useState("");
   const goToSignInPage = () => {
     router.push("/user/login");
     return handleCloseNavbar();
   };
   const handleLogout = async () => {
     await DeleteBiscuitCookie("accessToken");
-    if (typeof window !== "undefined") {
-      localStorage.setItem("isUserLogined", "false");
-    }
+    deleteCookie("uid");
+
     return goToSignInPage();
   };
+
   useEffect(() => {
-    const fetchUsers = async () => {
-      try {
-        const user = await axios.get(
-          "http://localhost:5173/api/users/getUsers"
-        );
-        // console.log(user?.data?.data);
-        setCurrentUser(user?.data?.data);
-      } catch (error: any) {
-        console.log(error.message);
-      }
-    };
-    fetchUsers();
+    const uid = getCookie("uid") as string;
+    console.log(uid);
   }, []);
-  const uid = getCookie("uid");
-  // console.log(uid);
-  const isCurrentUserLogin = currentUser?.map((user) => {
-    return user?._id === uid;
-  });
-  console.log(isCurrentUserLogin);
   return (
     <>
       <Link onClick={handleCloseNavbar} href="/" className={linkClass}>
@@ -74,13 +55,23 @@ function NavItems({ handleCloseNavbar }: { handleCloseNavbar: () => void }) {
         About
       </Link>
 
-      <Button
-        onClick={goToSignInPage}
-        variant="default"
-        className="select-none  py-2 text-sm text-white"
-      >
-        Sign In
-      </Button>
+      {true ? (
+        <Button
+          onClick={goToSignInPage}
+          variant="default"
+          className="select-none  py-2 text-sm text-white"
+        >
+          Sign In
+        </Button>
+      ) : (
+        <Button
+          onClick={handleLogout}
+          variant="outline"
+          className="select-none  py-2 text-sm border-foreground text-foreground"
+        >
+          Logout
+        </Button>
+      )}
     </>
   );
 }
