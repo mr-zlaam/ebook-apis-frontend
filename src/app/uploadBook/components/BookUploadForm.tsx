8c6c23517ba482c024d03ecb634dcb146e4e4bec
Zlaam <mrzalaam@gmail.com>
2024-04-27 21:39:41 +0500

    update BookUploadForm.tsx

--- src/app/uploadBook/components/BookUploadForm.tsx
+++ src/app/uploadBook/components/BookUploadForm.tsx
@@ -4,9 +4,10 @@ import {} from "react";
 function BookUploadForm() {
   const handleUploadBook = async () => {};
   return (
-    <>
+    <section>
+      <h1 className="text-center font-bold text-3xl my-3">Upload Book</h1>
       <form onSubmit={handleUploadBook}></form>
-    </>
+    </section>
   );
 }
 
